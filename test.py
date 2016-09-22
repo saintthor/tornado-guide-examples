d0ee1a00b4165ca31e2703d45ecdc28d3d35fa75
thor <zhuo.liu@newbeeair.com>
2016-09-22 16:10:57 +0800

    test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+@author: thor
+"""
+from tornado.testing import gen_test, AsyncTestCase
+from tornado.httpclient import AsyncHTTPClient
+import unittest
+
+class MyAsyncTest( AsyncTestCase ):
+    @gen_test
+    def test_ResetPswd( self ):
+        client = AsyncHTTPClient( self.io_loop )
+        path = 'http://localhost:8765/example?delay=2'
+        responses = yield [client.fetch( path, method = 'GET' ) for _ in range( 10 )]
+        for response in responses:
+            print response.body
+        
+if __name__ == '__main__':
+    unittest.main()

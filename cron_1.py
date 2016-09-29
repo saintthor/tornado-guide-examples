23600a916f9ca47f47725181e12eea622b13a47a
thor <zhuo.liu@newbeeair.com>
2016-09-29 15:18:41 +0800

    add cron_1.py

---
+++ cron_1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 26 14:32:52 2016
+
+@author: thor
+"""
+
+from tornado import ioloop, gen
+from time import time
+
+@gen.coroutine
+def Count():
+    print 'it\'s time to get up.'
+    
+if __name__ == '__main__':
+    loop = ioloop.IOLoop.current()
+    loop.call_at( time() + 5, Count )
+    loop.start()
+
